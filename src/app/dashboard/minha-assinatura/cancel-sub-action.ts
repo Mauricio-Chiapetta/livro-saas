6952c9eb7ddf260e550f7cea5688f2b832f49d47
Mauricio Chiapetta <m.faisca1234@gmail.com>
2025-06-27 01:07:35 -0300

    fix: corrigir a capitalização do parâmetro formData na função cancelSubscriptionAction

--- src/app/dashboard/minha-assinatura/cancel-sub-action.ts
+++ src/app/dashboard/minha-assinatura/cancel-sub-action.ts
@@ -1,10 +1,10 @@
 "use server";
 import { revalidatePath } from "next/cache";
 import Stripe from "stripe";
-export async function cancelSubscriptionAction(FormData: FormData) {
+export async function cancelSubscriptionAction(formData: FormData) {
   const stripe = new Stripe(process.env.STRIPE_SECRET_KEY ?? "");
 
-  const subscriptionId = FormData.get("subscriptionId") as string;
+  const subscriptionId = formData.get("subscriptionId") as string;
 
   stripe.subscriptions.cancel(subscriptionId);
 
